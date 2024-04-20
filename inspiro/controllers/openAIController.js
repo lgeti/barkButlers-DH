eb83c33083804ded8b17c108be0bf6d6f53fc7f8
igeti <ignat.pocevski@gmail.com>
2024-04-20 21:51:16 +0200

    face detection API implemented

--- inspiro/controllers/openAIController.js
+++ inspiro/controllers/openAIController.js
@@ -12,6 +12,7 @@ async function main(req, res, next) {
     try {
         const userMessage = req.body.message;
         const urnik = req.body.urnik;
+        const imageUrl = "https://i.pinimg.com/736x/3d/90/7f/3d907f72a577dfcb702751b047e201c5.jpg";
 
         async function sendMessageToAI(userMessage) {
             const messages = [
@@ -34,9 +35,37 @@ async function main(req, res, next) {
             return aiContent;
         }
 
-        const aiResponse = await sendMessageToAI(userMessage);
+        async function faceDetector(imageUrl) {
+            const apiKey = 'r6ggfvdal68rs118rcmacn3a7v';
+            const apiSecret = 'q9ojaqej6oejh587i3k9jeu0p8';
+            const attributes = 'all';
 
-        res.json({ aiResponse, urnik });
+            const url = `https://api.skybiometry.com/fc/faces/detect.json?api_key=${apiKey}&api_secret=${apiSecret}&urls=${imageUrl}&attributes=${attributes}`;
+
+            const response = await fetch(url, {
+                method: 'POST'
+            });
+            
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+        
+            const data = await response.json();
+            console.log(data);
+
+            return data;
+        
+            // if (data.status === 'success') {
+            //     return data.photos[0].tags; // this will be an array of detected faces
+            // } else {
+            //     throw new Error(data.error_message);
+            // }
+        }
+
+        const aiResponse = await sendMessageToAI(userMessage);
+        const faceDetectorResponse = await faceDetector(imageUrl);
+
+        res.json({ aiResponse, urnik, faceDetectorResponse });
     }
     catch (error) {
         next(error);
