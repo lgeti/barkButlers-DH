221fd31ba71533d1644e5c71a9ff5ebdb1362525
igeti <ignat.pocevski@gmail.com>
2024-04-20 16:22:51 +0200

    Add multer package for file uploads

--- inspiro/routes/index.js
+++ inspiro/routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/upload', function(req, res, next) {
+  res.send(`
+    <form action="/upload" method="post" enctype="multipart/form-data">
+      <input type="file" name="myFile">
+      <button type="submit">Upload</button>
+    </form>
+  `);
+});
+
+router.post('/upload', upload.single('myFile'), function(req, res, next) {
+  // req.file is the 'myFile' file
+  // req.body will hold the text fields, if there were any
+  res.send('File uploaded!');
 });
 
 module.exports = router;
